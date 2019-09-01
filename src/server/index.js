d52ea5e2b7cd91bd9fa383cbf0c05576923c44d6
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-09-01 17:08:20 -0500

    Add Winston for log system

--- src/server/index.js
+++ src/server/index.js
@@ -4,22 +4,30 @@ const api = require("./api/v1");
 
 const app = express();
 
+const logger = require("./config/logger");
+
 app.use("/api/v1", api);
 app.use("/api", api);
 
 // Not route found middleware
 
 app.use((req, res, next) => {
-  res.status(404);
+  const message = "Route not found";
+  const statusCode = 404;
+
+  logger.warn(message);
+
+  res.status(statusCode);
   res.json({
-    message: "Resource not found",
+    message,
   });
 });
 
 // Error middleware
-
 app.use((err, req, res, next) => {
   const { message, statusCode = 500 } = err;
+
+  logger.error(message);
 
   res.status(statusCode);
   res.json({
