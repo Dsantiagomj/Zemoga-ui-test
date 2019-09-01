fac96d724317b96ce5bc1fd9fc408aca5465f9d4
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-09-01 18:10:59 -0500

    mongoose and mongo db setup

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 
 const config = {
   server: {
-    port: process.env.SERVER_PORT || 5000,
+    port: process.env.SERVER_PORT || 3000,
+  },
+  database: {
+    protocol: process.env.DATABASE_PROTOCOL,
+    url: process.env.DATABASE_URL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
   },
 };
 
