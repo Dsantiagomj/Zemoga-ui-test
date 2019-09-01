47c04c2ab4376e308b33deaa77bb49f56283462c
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-09-01 18:32:11 -0500

    CRUD with Router.param

--- src/server/api/v1/candidates/controller.js
+++ src/server/api/v1/candidates/controller.js
@@ -2,6 +2,22 @@ const HTTP_STATUS_CODE = require("http-status-codes");
 
 const Model = require("./model");
 
+exports.id = (req, res, next, id) => {
+  Model.findById(id).exec((err, doc) => {
+    if (err) {
+      next(err);
+    } else if (doc) {
+      req.doc = doc;
+      next();
+    } else {
+      next({
+        message: "Resource not found",
+        statusCode: HTTP_STATUS_CODE.NOT_FOUND,
+      });
+    }
+  });
+};
+
 exports.create = (req, res, next) => {
   const { body = {} } = req;
 
@@ -34,54 +50,45 @@ exports.all = (req, res, next) => {
 };
 
 exports.read = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
+  const { doc } = req;
 
-  Model.findById(id).exec((err, doc) => {
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
+  });
+};
+
+exports.update = (req, res, next) => {
+  const { body = {}, doc } = req;
+
+  Object.assign(doc, body);
+
+  doc.save((err, updated) => {
     if (err) {
       next(err);
-    } else if (doc) {
+    } else {
       res.json({
-        data: doc,
+        data: updated,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
-      });
-    } else {
-      next({
-        message: "Resource not found",
-        statusCode: HTTP_STATUS_CODE.NOT_FOUND,
       });
     }
   });
 };
 
-exports.update = (req, res, next) => {
-  const { body = {}, params = {} } = req;
-  const { id } = params;
-
-  const doc = {
-    ...body,
-    id,
-  };
-
-  res.json({
-    data: doc,
-    success: true,
-    statusCode: HTTP_STATUS_CODE.OK,
-  });
-};
-
 exports.delete = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
+  const { doc } = req;
 
-  const doc = {
-    id,
-  };
-
-  res.json({
-    data: doc,
-    success: true,
-    statusCode: HTTP_STATUS_CODE.OK,
+  doc.remove((err, deleted) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json({
+        data: deleted,
+        success: true,
+        statusCode: HTTP_STATUS_CODE.OK,
+      });
+    }
   });
 };
