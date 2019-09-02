3847350ceca0987977710e1045c51968a41cf026
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-09-01 20:29:23 -0500

    Add HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/app/index.js",
   output: {
@@ -13,4 +15,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/index.html",
+    }),
+  ],
 };
