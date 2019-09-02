7c9c9c79c2de99e9d053d9871a2b58097d85371f
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-09-02 16:31:02 -0500

    add responsive navbar functionality

--- src/app/components/navbar/index.js
+++ src/app/components/navbar/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
-
-import Search from "../../../public/assets/search.svg";
-import Burguer from "../../../public/assets/burger.svg";
-
 import "./style.css";
 
+import Search from "../../../public/assets/search.svg";
+import Burger from "../../../public/assets/burger.svg";
+import Close from "../../../public/assets/white_close.svg";
+
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.navbar = React.createRef();
+    this.burger = React.createRef();
+  }
+
+  handleMenu = () => {
+    this.navbar.current.classList.toggle("active__block");
+    if (this.navbar.current.classList.contains("active__block")) {
+      this.burger.current.setAttribute("src", Close);
+    } else {
+      this.burger.current.setAttribute("src", Burger);
+    }
+  };
+
   render() {
     return (
       <div className="navbar">
@@ -14,24 +30,27 @@ export default class Navbar extends Component {
             <h1 className="navbar__title">Rule of Thumb</h1>
           </div>
         </nav>
-        <nav className="navbar__section navbar__section--right">
+        <nav
+          className="navbar__section navbar__section--right"
+          ref={this.navbar}
+        >
           <div className="navbar__item">
-            <a className="navbar__link" href="localhost:3000">
+            <a className="navbar__link" href="#">
               Past Trials
             </a>
           </div>
           <div className="navbar__item">
-            <a className="navbar__link" href="localhost:3000">
+            <a className="navbar__link" href="#">
               How it works
             </a>
           </div>
           <div className="navbar__item">
-            <a className="navbar__link" href="localhost:3000">
+            <a className="navbar__link" href="#">
               Log In/ Sign Up
             </a>
           </div>
           <div className="navbar__item">
-            <a className="navbar__link" href="localhost:3000">
+            <a className="navbar__link" href="#">
               <img
                 className="navbar__icon search__image"
                 src={Search}
@@ -40,11 +59,16 @@ export default class Navbar extends Component {
             </a>
           </div>
         </nav>
-        <div id="burger" className="navbar__section navbar__section--toggle">
+        <div
+          id="burger"
+          className="navbar__section navbar__section--toggle"
+          onClick={() => this.handleMenu()}
+        >
           <img
             className="navbar__icon burger__image"
-            src={Burguer}
+            src={Burger}
             alt="burger"
+            ref={this.burger}
           />
         </div>
       </div>
